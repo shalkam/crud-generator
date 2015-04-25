5c04a0a1bfab623638d29989a8b8179e93a05617
mostafa <mostafa@mostafa-Aspire-5742G>
2015-04-25 14:30:05 +0200

    added base tag to header

--- src/Shalkam/CrudGenerator/views/app.blade.php
+++ src/Shalkam/CrudGenerator/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>AdminLTE 2 | Dashboard</title>
+        <base href="{{Request::url()}}/">
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
         <!-- Bootstrap 3.3.2 -->
         <link href="{{ asset('adminlte/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
